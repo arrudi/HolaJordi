fa204b13eae09f80296a4b43e47bada9aca3420f
Felipe Arrudi <felipe.arrudi@gmail.com>
2022-12-14 20:24:27 +0000

    a evolución

--- HolaJordi/ContentView.swift
+++ HolaJordi/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Jordi!")
+            Text("Hello, Pau!")
         }
         .padding()
     }
