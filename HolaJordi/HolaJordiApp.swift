98cdf92c9b0875763b15516d1cc7e2f9e3648835
Felipe Arrudi <felipe.arrudi@gmail.com>
2022-12-14 20:15:42 +0000

    Initial Commit

---
+++ HolaJordi/HolaJordiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolaJordiApp.swift
+//  HolaJordi
+//
+//  Created by Felipe Arrudi on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct HolaJordiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
